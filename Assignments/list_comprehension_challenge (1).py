e4f024826d318f1c2a2b763efa738f4b9f8fdbb2
Anuj Pandey <118086297+newguy7@users.noreply.github.com>
2024-03-27 14:42:48 -0700

    List Comprehension
     python program to print a list with all numbers less than 100 who are divisible by both 2 and 5

---
+++ Assignments/list_comprehension_challenge (1).py
@@ -0,0 +1,8 @@
+"Write a program to print a list with all numbers less than 100 who are divisible by both 2 and 5?"
+
+
+def div_by_2_and_5():
+    my_list = [x for x in range(100) if x%2 == 0 and x%5 == 0]
+    return my_list
+
+print(div_by_2_and_5())
